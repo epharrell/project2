ae8759e38798656a7ff88bcdd19069fd35a1d9bd
dummy <dummy@dummy.ie>
2021-02-18 10:20:22 +0000

    Added enhanced for loop

--- GroupProject2/src/test/Test.java
+++ GroupProject2/src/test/Test.java
@@ -4,12 +4,12 @@ public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
 		
-		int arr[] = {2, 6, 8, 10};
+		int arr[] = {2, 6, 8, 10, 12, 14};
 		
 		//Adding some new stuff
 		int sum = 0;
-		for(int i=0; i<10; i++) {
-			sum += arr[i];
+		for(int i : arr) {
+			sum += i;
 		}
 		System.out.println("Sum is: " + sum);
 	}
