51fadfc9179db5d8e84dccadf2891ba74ef57c3f
dummy <dummy@dummy.ie>
2021-02-18 10:15:40 +0000

    Added an array

--- GroupProject2/src/test/Test.java
+++ GroupProject2/src/test/Test.java
@@ -4,10 +4,12 @@ public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
 		
+		int arr[] = {2, 6, 8, 10};
+		
 		//Adding some new stuff
 		int sum = 0;
 		for(int i=0; i<10; i++) {
-			sum+=i;
+			sum += arr[i];
 		}
 		System.out.println("Sum is: " + sum);
 	}
