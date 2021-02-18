fd22a052a3c7e026d7d05e6fb7f526a5526696f3
dummy <dummy@dummy.ie>
2021-02-18 10:22:28 +0000

    Added the average calculation

--- GroupProject2/src/test/Test.java
+++ GroupProject2/src/test/Test.java
@@ -8,9 +8,10 @@ public class Test {
 		
 		//Adding some new stuff
 		int sum = 0;
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<arr.length; i++) {
 			sum += arr[i];
 		}
 		System.out.println("Sum is: " + sum);
+		System.out.println("Average is: " + sum/4);
 	}
 }
